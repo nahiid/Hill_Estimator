551c76fe923c0c7d6435ddf2951448d53e188cb0
Nahid <nahid.aut@gmail.com>
2023-10-13 13:45:52 -0400

    Add line to the hill plot

--- Assn3/hill_estimator.R
+++ Assn3/hill_estimator.R
@@ -9,18 +9,29 @@ library("evir")
 o2 <- hill(danish)
 detach("package:evir")
 
-
-
-#Problem 1
+######################
+##### Problem 1 ######
+######################
 
 set.seed(123)
 
-# Model A : generate standard Cauchy data
+############
+#### Model A : generate standard Cauchy data
 cauchy_data = rcauchy(1000)
 
-# Plot data
-plot(cauchy_data, xlab = "t", ylab = "X_t")
+# Scatter Plot 
+plot(cauchy_data, xlab = "t", ylab = "X_t", main="Standard Cauchy Data Plot")
+
+# Box Plot
+boxplot(cauchy_data, main = "Box Plot of Cauchy Data", ylab = "X_t")
 
 # Hill plot
 library(evir)
 hill(cauchy_data)
+
+# Add line to hill plot
+abline(h = 1, col = "pink")
+
+############
+#### Model B : generate standard Cauchy data
+
